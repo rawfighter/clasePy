039f2af3960d05124dac2e92f923fe0207d9f5c3
Ronald De Santiago <masteraqu2les@gmail.com>
2019-06-03 03:41:50 -0700

    1st Git Push

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+fruitsByColor = {"red": "apple", "blue": "berry"}
+#!retrieve the values
+name=""
+print(fruitsByColor["blue"])
+while(name != "-done"):
+    print("Enter student name to get theirs hobbies")
+    name= input()
+    hobbiesByPerson={"Aaron":["photography","Speluking"], "Sarah":["climbing", "Speluking"]}
+    if (name != "-done"):
+         hobbies = hobbiesByPerson[name]
+    for h in hobbies:
+        print(h)
+print("Acabamos")   
